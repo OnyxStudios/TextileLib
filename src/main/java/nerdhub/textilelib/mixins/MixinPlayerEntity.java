c70ee6490ed2a2b42935633969172f823334e3d9
abused_master <techperson71@gmail.com>
2019-01-11 20:12:07 -0500

    Added more capability to TickEvents

--- src/main/java/nerdhub/textilelib/mixins/MixinPlayerEntity.java
+++ src/main/java/nerdhub/textilelib/mixins/MixinPlayerEntity.java
@@ -2,6 +2,7 @@ package nerdhub.textilelib.mixins;
 
 import nerdhub.textilelib.eventhandlers.EventRegistry;
 import nerdhub.textilelib.events.PlayerEvents;
+import nerdhub.textilelib.events.TickEvents;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
@@ -24,6 +26,12 @@ public abstract class MixinPlayerEntity {
             cir.setReturnValue(ActionResult.FAILURE);
             cir.cancel();
         }
+    }
+
+    @Inject(method = "update", at = @At("HEAD"))
+    public void update(CallbackInfo ci) {
+        TickEvents.PlayerTickEvent playerTickEvent = new TickEvents.PlayerTickEvent((PlayerEntity) (Object) this);
+        EventRegistry.runEvent(playerTickEvent);
     }
 
     @Shadow

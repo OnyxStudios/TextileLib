04c5a4afa20f429054ad01273bd4886e01aae7f5
CJ Miller <cjmiller721@gmail.com>
2019-01-11 20:45:40 -0800

    Support lambdas for registering

--- src/main/java/nerdhub/textilelib/eventhandlers/EventRegistry.java
+++ src/main/java/nerdhub/textilelib/eventhandlers/EventRegistry.java
@@ -7,17 +7,20 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class EventRegistry {
 
     public static EventRegistry INSTANCE = new EventRegistry();
 
     private final Map<Method, Object> eventSubscriberMethods;
-    private final Multimap<Class, Method> classMethodMultimap;
+    private final Multimap<Class<? extends Event>, Method> classMethodMultimap;
+    private final Multimap<Class<? extends Event>, Consumer<? extends Event>> classLambdaMultimap;
 
     private EventRegistry() {
         eventSubscriberMethods = new HashMap<>();
         classMethodMultimap = MultimapBuilder.hashKeys().arrayListValues().build();
+        classLambdaMultimap = MultimapBuilder.hashKeys().arrayListValues().build();
     }
 
     public void registerEventHandler(Object clazz) {
@@ -25,14 +28,12 @@ public class EventRegistry {
 
             if(method.isAnnotationPresent(EventSubscriber.class)) {
                 if (method.getParameters().length > 1) {
-                    throw new UnsupportedOperationException("Cannot have multiple parameters on a @EventSubscriber method! " + method.getName());
+                    throw new UnsupportedOperationException("Cannot have multiple parameters on a @EventSubscriber method! Method Name: " + method.getName());
                 }
 
                 for (Class paramClass : method.getParameterTypes()) {
-                    try {
-                        paramClass.asSubclass(Event.class);
-                    } catch (ClassCastException ex) {
-                        throw new UnsupportedOperationException("Methods annotated with EventSubscriber argument must be a subtype of the Event interface", ex);
+                    if(!implementsEvent(paramClass)) {
+                        throw new UnsupportedOperationException("Event handler method's arguments must be a subtype of the Event interface. Method Name: " + method.getName());
                     }
                     eventSubscriberMethods.put(method, clazz);
                     classMethodMultimap.put(paramClass, method);
@@ -41,9 +42,17 @@ public class EventRegistry {
         }
     }
 
-    public void fireEvent(Event event) {
+    public <E extends Event> void registerEventHandler(Consumer<E> handler, Class<E> eventClass) {
+        classLambdaMultimap.put(eventClass, handler);
+    }
+
+    public <T extends Event> void fireEvent(T event) {
         for (Method method : classMethodMultimap.get(event.getClass())) {
             invokeMethod(eventSubscriberMethods.get(method), method, event);
+        }
+
+        for(Consumer<? extends Event> consumer: classLambdaMultimap.get(event.getClass())) {
+            ((Consumer<T>)consumer).accept(event);
         }
     }
 
@@ -51,7 +60,14 @@ public class EventRegistry {
         for (Method method : classMethodMultimap.get(event.getClass())) {
             invokeMethod(eventSubscriberMethods.get(method), method, event);
             if(event.isCanceled()) {
-                break;
+                return;
+            }
+        }
+
+        for(Consumer<? extends Event> consumer: classLambdaMultimap.get(event.getClass())) {
+            ((Consumer<T>)consumer).accept(event);
+            if(event.isCanceled()) {
+                return;
             }
         }
     }
@@ -64,5 +80,15 @@ public class EventRegistry {
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean implementsEvent(Class clazz) {
+        do {
+            if(clazz == Event.class) {
+                return true;
+            }
+            clazz = clazz.getSuperclass();
+        } while (clazz != null);
+        return false;
     }
 }

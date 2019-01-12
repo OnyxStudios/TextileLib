4c0570068d479ebbb7f531b5705e679436e362f4
abused_master <techperson71@gmail.com>
2019-01-12 00:17:27 -0500

    Remove MODID from main class

--- src/main/java/nerdhub/textilelib/TextileLib.java
+++ src/main/java/nerdhub/textilelib/TextileLib.java
@@ -4,8 +4,6 @@ import net.fabricmc.api.ModInitializer;
 
 public class TextileLib implements ModInitializer {
 
-    public static final String MODID = "textilelib";
-
     @Override
     public void onInitialize() {
     }

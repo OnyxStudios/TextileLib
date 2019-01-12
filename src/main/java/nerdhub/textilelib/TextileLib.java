5527275376a709a37d624117ce498cdef579cad2
abused_master <techperson71@gmail.com>
2019-01-12 00:15:38 -0500

    Removed Logger from main class

--- src/main/java/nerdhub/textilelib/TextileLib.java
+++ src/main/java/nerdhub/textilelib/TextileLib.java
@@ -1,12 +1,10 @@
 package nerdhub.textilelib;
 
 import net.fabricmc.api.ModInitializer;
-import java.util.logging.Logger;
 
 public class TextileLib implements ModInitializer {
 
     public static final String MODID = "textilelib";
-    public static Logger LOGGER = Logger.getLogger("TextileLib");
 
     @Override
     public void onInitialize() {

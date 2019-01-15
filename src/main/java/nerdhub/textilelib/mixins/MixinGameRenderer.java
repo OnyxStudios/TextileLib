5b8724d9796ee943868ae4c198f8dd8268996baf
UpcraftLP <j-schirra@gmx.de>
2019-01-15 01:43:15 +0100

    fix mixin injection

--- src/main/java/nerdhub/textilelib/mixins/MixinGameRenderer.java
+++ src/main/java/nerdhub/textilelib/mixins/MixinGameRenderer.java
@@ -4,11 +4,8 @@ import nerdhub.textilelib.eventhandlers.EventRegistry;
 import nerdhub.textilelib.events.RenderWorldEvent;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
@@ -19,7 +16,7 @@ public class MixinGameRenderer {
     private MinecraftClient client;
 
     //TODO Map to renderWorld
-    @Inject(method = "method_3178", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", ordinal = 17, shift = At.Shift.BEFORE))
+    @Inject(at = @At(value = "INVOKE_STRING", target = "Lnet/minecraft/util/profiler/Profiler;swap(Ljava/lang/String;)V", args = "ldc=entities", shift = At.Shift.BEFORE), method = "method_3178")
     private void method_3178(float float_1, long long_1, CallbackInfo ci) {
         this.client.getProfiler().swap("textilelib_render_world");
         RenderWorldEvent renderWorldEvent = new RenderWorldEvent(this.client.renderer, float_1);

2e097bf2d107a33d68af430a2df5ede9d882bf03
abused_master <techperson71@gmail.com>
2019-01-15 08:55:17 -0500

    Fixing mixin injection class

--- src/main/java/nerdhub/textilelib/mixins/MixinGui.java
+++ src/main/java/nerdhub/textilelib/mixins/MixinGui.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MixinGui.class)
+@Mixin(Gui.class)
 public class MixinGui {
 
     @Inject(method = "draw", at = @At("RETURN"))

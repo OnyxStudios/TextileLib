9af697594bec49d67fd9befddd1a47a576fd133d
UpcraftLP <j-schirra@gmx.de>
2019-02-17 08:13:12 +0100

    update to 19w07a

--- src/main/java/nerdhub/textilelib/mixins/MixinClientPlayerInteractionManager.java
+++ src/main/java/nerdhub/textilelib/mixins/MixinClientPlayerInteractionManager.java
@@ -16,6 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ClientPlayerInteractionManager.class)
 public abstract class MixinClientPlayerInteractionManager {
 
+    @SuppressWarnings("Duplicates")
     @Inject(method = "interactBlock", at = @At("HEAD"), cancellable = true)
     private void interactBlock(ClientPlayerEntity clientPlayerEntity_1, ClientWorld clientWorld_1, Hand hand_1, BlockHitResult blockHitResult_1, CallbackInfoReturnable<ActionResult> cir) {
         PlayerInteractBlockEvent interactBlockEvent = new PlayerInteractBlockEvent(clientPlayerEntity_1, hand_1, clientWorld_1.getBlockState(blockHitResult_1.getBlockPos()), blockHitResult_1.getBlockPos());
--- src/main/java/nerdhub/textilelib/mixins/MixinServerPlayerInteractionManager.java
+++ src/main/java/nerdhub/textilelib/mixins/MixinServerPlayerInteractionManager.java
@@ -3,7 +3,7 @@ package nerdhub.textilelib.mixins;
 import nerdhub.textilelib.eventhandlers.EventRegistry;
 import nerdhub.textilelib.events.block.BlockBreakEvent;
 import nerdhub.textilelib.events.entity.player.PlayerInteractBlockEvent;
-import net.minecraft.client.network.packet.BlockUpdateClientPacket;
+import net.minecraft.client.network.packet.BlockUpdateS2CPacket;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -36,11 +36,12 @@ public abstract class MixinServerPlayerInteractionManager {
             cir.cancel();
             // Update client as they will believe they have broken the block.
             if(player != null) {
-                this.player.networkHandler.sendPacket(new BlockUpdateClientPacket(world, blockPos_1));
+                this.player.networkHandler.sendPacket(new BlockUpdateS2CPacket(world, blockPos_1));
             }
         }
     }
 
+    @SuppressWarnings("Duplicates")
     @Inject(method = "interactBlock", at = @At("HEAD"), cancellable = true)
     private void interactBlock(PlayerEntity playerEntity_1, World world_1, ItemStack itemStack_1, Hand hand_1, BlockHitResult blockHitResult_1, CallbackInfoReturnable<ActionResult> cir) {
         PlayerInteractBlockEvent interactBlockEvent = new PlayerInteractBlockEvent(playerEntity_1, hand_1, world_1.getBlockState(blockHitResult_1.getBlockPos()), blockHitResult_1.getBlockPos());
